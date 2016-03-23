de9320e8a049677e4da5b9d329dc98002333301d
vika L <vika.lapsar@gmail.com>
2016-03-23 18:12:23 +0200

    adding comments

--- meteor/Meteor.js/siteace/client/comments/comments.js
+++ meteor/Meteor.js/siteace/client/comments/comments.js
@@ -1,14 +1,13 @@
 Template.comments_form.events({
 		"submit .js-post-comment-form":function(event){
 
-			var comment = event.target.comment.value;
-			console.log(comment);
 			var page_id = this._id;
+			var date = new Date().toJSON().slice(0,19);
 			Comments.insert({
 				name:event.target.name.value, 
 				comment:event.target.comment.value, 
 				webpage_id: page_id,
-				createdOn:new Date()
+				createdOn: date
     	    });
 			$('.js-post-comment-form').trigger("reset");
 			return false;// stop the form submit from reloading the page
@@ -18,6 +17,6 @@ Template.comments_form.events({
 	
 	Template.comments_list.helpers({
 		comments:function(){
-			return Comments.find({}, {sort:{createdOn:-1}}, {});
+			return Comments.find({webpage_id:this._id}, {sort:{createdOn:-1}}, {});
 		}
 	});
