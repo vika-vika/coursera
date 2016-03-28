ce8f44fb3c5dc0a75e7488220e350d8e234bc950
vika L <vika.lapsar@gmail.com>
2016-03-28 18:06:14 +0300

    >>Minstant  left chat pane added

--- meteor.js/minstant/client/minstant.js
+++ meteor.js/minstant/client/minstant.js
@@ -21,6 +21,27 @@
     }
   })
 
+  Template.available_user.events({
+    'click .js-user-click': function(event){
+        var otherUserId = this._id;
+		// find a chat that has two users that match current user id
+		// and the requested user id
+		var filter = {$or:[
+                {user1Id:Meteor.userId(), user2Id:otherUserId}, 
+                {user2Id:Meteor.userId(), user1Id:otherUserId}
+                ]};
+		var chat = Chats.findOne(filter);
+		if (!chat){// no chat matching the filter - need to insert a new one
+			chatId = Chats.insert({user1Id:Meteor.userId(), user2Id:otherUserId});
+		}
+		else {// there is a chat going already - use that. 
+			chatId = chat._id;
+		}
+		if (chatId){// looking good, save the id to the session
+		Session.set("chatId",chatId);
+		}
+    }
+});
 
   Template.chat_page.helpers({
     messages:function(){
